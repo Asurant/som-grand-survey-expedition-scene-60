0f2f4ce8d6dd88e4c451c585600fb3d8c843d57e
Asurant <149097598+Asurant@users.noreply.github.com>
2025-09-27 21:20:36 -0400

    Basic Setup

---
+++ index.js
@@ -0,0 +1,11 @@
+let liftButton = document.getElementById("liftButton")
+let swoleCoinDisplay = document.getElementById("swoleCoinDisplay")
+
+let increaseWeight = document.getElementById("increaseWeight")
+let gymBro = document.getElementById("gymBro")
+
+let swoleCoins = 0;
+let multiplier = 1;
+let multiplierCost = 25;
+let gymBros = 0;
+let gymBroCost = 15;
