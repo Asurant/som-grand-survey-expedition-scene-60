31f0fe363bb19e1593a0256ad08d258191229eca
Asurant <149097598+Asurant@users.noreply.github.com>
2025-09-27 21:47:59 -0400

    Base Clicker Game

--- index.js
+++ index.js
@@ -8,4 +8,42 @@ let swoleCoins = 0;
 let multiplier = 1;
 let multiplierCost = 25;
 let gymBros = 0;
-let gymBroCost = 15;
+let gymBroCost = 50;
+
+function displaySwoleCoinAmount(){
+    swoleCoinDisplay.innerText = "You have " + Math.round(swoleCoins) + " Swole Coins";
+}
+
+liftButton.addEventListener('click', function(){
+    swoleCoins += multiplier;
+    displaySwoleCoinAmount()
+})
+
+increaseWeight.addEventListener('click', function(){
+    if(swoleCoins>= multiplierCost){
+        swoleCoins-= multiplierCost;
+        multiplierCost*=1.3;
+        multiplier++;
+        displaySwoleCoinAmount()
+        increaseWeight.innerText = "Increase weight for " + Math.round(multiplierCost) + " Swole Coins";
+    }else{
+        alert("Not Enough Swole Coins")
+    }
+})
+
+gymBro.addEventListener('click', function(){
+    if(swoleCoins>= gymBroCost){
+        swoleCoins-= gymBroCost;
+        gymBroCost*=1.3;
+        gymBros++;
+        displaySwoleCoinAmount()
+        gymBro.innerText = "Get some gym bros for " + Math.round(gymBroCost) + " Swole Coins";
+    }else{
+        alert("Not Enough Swole Coins")
+    }
+})
+
+setInterval(function(){
+    swoleCoins += gymBros
+    displaySwoleCoinAmount()
+},1000)
